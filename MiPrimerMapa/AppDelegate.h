404dd793ff07fb786dadb1aa497d1563aaea1c1e
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-09-27 18:37:25 -0500

    Initial Commit

---
+++ MiPrimerMapa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiPrimerMapa
+//
+//  Created by Marco Antonio Roa De La Cruz on 9/27/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
