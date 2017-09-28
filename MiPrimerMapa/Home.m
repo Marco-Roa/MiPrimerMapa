64485be05849ea4d5dba2b422944aa5c4ac523c7
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-09-27 19:44:50 -0500

    Change camera and pin location

--- MiPrimerMapa/Home.m
+++ MiPrimerMapa/Home.m
@@ -28,18 +28,18 @@
 - (void)loadView {
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:20.694702
+                                                            longitude:-103.418137
+                                                                 zoom:16];
     GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView.myLocationEnabled = YES;
     self.view = mapView;
     
     // Creates a marker in the center of the map.
     GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
+    marker.position = CLLocationCoordinate2DMake(20.694702, -103.418137);
+    marker.title = @"UAG";
+    marker.snippet = @"Jalisco";
     marker.map = mapView;
 }
 
